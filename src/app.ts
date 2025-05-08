d3115393b962e052c12d7393277e155316d14a2d
Eduardo Ferraz <edupferraz@gmail.com>
2025-05-08 07:20:45 -0300

    feat: implement user creation with controllers, repositories and services

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,24 @@
 import fastify from 'fastify'
-import { PrismaClient } from 'generated/prisma'
+import { appRoutes } from '@/http/routes'
+import { ZodError } from 'zod'
+import { env } from './env'
 
 export const app = fastify()
 
-const prisma = new PrismaClient()
+app.register(appRoutes)
 
-prisma.user.create({
-  data: {
-    name: 'Eduardo Ferraz',
-    email: 'eduardo.ferraz@gmail.com.br',
-  },
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error.', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // TODO: Here we should log to a external tool like DataDog/NewRelic/Sentry
+  }
+
+  return reply.status(500).send({ message: 'Internal server error.' })
 })
