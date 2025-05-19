f6552906ee01195fea65bcca654f549c8bcba43a
Eduardo Ferraz <edupferraz@gmail.com>
2025-05-19 18:43:50 -0300

    feat: validate check-in

---
+++ src/services/validate-check-in.ts
@@ -0,0 +1,33 @@
+import { CheckInsRepository } from '@/repositories/check-ins-repository'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { CheckIn } from 'generated/prisma'
+
+interface ValidateCheckInServiceRequest {
+  checkInId: string
+}
+
+interface ValidateCheckInServiceResponse {
+  checkIn: CheckIn
+}
+
+export class ValidateCheckInService {
+  constructor(private checkInsRepository: CheckInsRepository) { }
+
+  async execute({
+    checkInId,
+  }: ValidateCheckInServiceRequest): Promise<ValidateCheckInServiceResponse> {
+    const checkIn = await this.checkInsRepository.findById(checkInId)
+
+    if (!checkIn) {
+      throw new ResourceNotFoundError()
+    }
+
+    checkIn.validated_at = new Date()
+
+    await this.checkInsRepository.save(checkIn)
+
+    return {
+      checkIn,
+    }
+  }
+}
