ebf5b93f71cd6b6d77ca9aef5a8253ef04d4e010
Eduardo Ferraz <edupferraz@gmail.com>
2025-05-19 22:14:32 -0300

    test(e2e): create test environment

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,26 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    dir: 'src',
+    workspace: [
+      {
+        extends: true,
+        test: {
+          name: 'unit',
+          dir: 'src/services/tests',
+        },
+      },
+
+      {
+        extends: true,
+        test: {
+          name: 'e2e',
+          dir: 'src/http/controllers/tests',
+          environment:
+            './prisma/vitest-environment-prisma/prisma-test-environment.ts',
+        },
+      },
+    ],
+  },
 })
