ea114acd641f37230b8b96baa15b40268a55be3c
Eduardo Ferraz <edupferraz@gmail.com>
2025-05-19 21:53:27 -0300

    feat: verify jwt on request

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,3 +1,4 @@
+import { makeGetUserProfileService } from '@/services/factories/make-get-user-profile-service'
 import { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function profile(
@@ -6,7 +7,16 @@ export async function profile(
 ) {
   await request.jwtVerify()
 
-  console.log(request.user.sub)
+  const getUserProfile = makeGetUserProfileService()
 
-  return reply.status(200).send()
+  const { user } = await getUserProfile.execute({
+    userId: request.user.sub,
+  })
+
+  return reply.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined,
+    },
+  })
 }
